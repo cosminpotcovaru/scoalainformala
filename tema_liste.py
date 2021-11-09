c57b6e0bcb9db077a49246fbdace1d033984a4df
Cosmin Potcovaru <ko5m1nx7@gmail.com>
2021-11-10 01:34:12 +0200

    Corectie cod

--- tema_liste.py
+++ tema_liste.py
@@ -14,18 +14,18 @@ print(f'Lista descendenta este: {my_list}')
 
 #Afisarea numerelor pare din lista initiala
 
-nr_pare = list_asc[1], list_asc[3], list_asc[5], list_asc[7], list_asc[9]
+nr_pare = list_asc[1::2]
 
 print(f'Numerele pare din lista ordonata sunt: {nr_pare}')
 
 #Afisarea numerelor impare din lista initiala
 
-nr_impare = list_asc[0], list_asc[2], list_asc[4], list_asc[6], list_asc[8]
+nr_impare = list_asc[0::2]
 
 print(f'Numerele impare din lista ordonata sunt: {nr_impare}')
 
 #Afisarea multiplilor lui 3 din lista initiala
 
-mult_3 = list_asc[2], list_asc[5], list_asc[8]
+mult_3 = list_asc[2::3]
 
-print(f'Multiplii lui 3 lista ordonata sunt: {mult_3}')
+print(f'Multiplii lui 3 din lista ordonata sunt: {mult_3}')
